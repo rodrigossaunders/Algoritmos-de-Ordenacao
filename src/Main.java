ea614272d9c5746d78aec484f1bcb15b3194e55a
Rodrigo Sena Saunders <rodrigossaunders@gmail.com>
2021-10-03 23:40:42 -0300

    Classe Utilidades

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         int array[] = {5,4,3,12,2,1,15,14};
         System.out.print("Original: ");
-        printarArray(array);
+        Utilidades.printarArray(array);
         System.out.print("Ordenado: ");
-        printarArray(bubbleSort(array));
+        Utilidades.printarArray(bubbleSort(array));
     }
 
     public static int[] bubbleSort(int arr[]) {
@@ -22,11 +22,6 @@ public class Main {
         return arr;
     }
 
-    public static void printarArray(int arr[]) {
-        for(int i = 0; i < arr.length; i++) {
-            System.out.printf("%d ", arr[i]);
-        }
-        System.out.println();
-    }
+
 
 }
---
+++ src/Utilidades.java
@@ -0,0 +1,8 @@
+public class Utilidades {
+    public static void printarArray(int arr[]) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+        System.out.println();
+    }
+}
